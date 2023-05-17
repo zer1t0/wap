cdd7a4a419ce2aeff6cda2daefb6b8fc081e497d
Eloy <zer1t0ps@protonmail.com>
2023-05-17 21:58:39 +0200

    move selenium helpers into library

--- wap/helper.py
+++ wap/helper.py
@@ -154,3 +154,69 @@ def parse_requests_cookies(cookies) -> Dict[str, List[str]]:
             cks[cookie.name] = []
         cks[cookie.name].append(cookie.value)
     return cks
+
+
+
+def parse_selenium_cookies(cookies) -> Dict[str, List[str]]:
+    """Helper to parse the cookies retrieved for the
+    selenium browser, and generate cookies to be used
+    by wap.
+
+    Example:
+        >>> import wap
+        >>> from selenium import webdriver
+        >>> browser = webdriver.Chrome()
+        >>> browser.get("https://www.github.com")
+        >>> cookies = wap.parse_selenium_cookies(browser.get_cookies())
+    """
+    cks = {}
+    for cookie in cookies:
+        if cookie["name"] not in cks:
+            cks[cookie["name"]] = []
+        cks[cookie["name"]].append(cookie["value"])
+    return cks
+
+
+def extract_scripts_selenium(browser) -> List[str]:
+    """Helper to extract the javascript scripts paths or URL
+    from the selenium browser.
+
+    Example:
+        >>> import wap
+        >>> from selenium import webdriver
+        >>> browser = webdriver.Chrome()
+        >>> browser.get("https://www.github.com")
+        >>> scripts = wap.extract_scripts_selenium(browser)
+
+    """
+    scripts = []
+    script_tags = browser.find_elements_by_tag_name("script")
+    for script in script_tags:
+        src = script.get_attribute("src")
+        if src and not src.startswith("data:text/javascript;"):
+            scripts.append(src)
+
+    return scripts
+
+
+def extract_metas_selenium(browser) -> Dict[str, str]:
+    """Helper to extract the name and content of the meta tags
+    from the selenium browser.
+
+    Example:
+        >>> from selenium import webdriver
+        >>> browser = webdriver.Chrome()
+        >>> browser.get("https://www.github.com")
+        >>> metas = extract_metas_selenium(browser)
+
+    """
+    meta_tags = browser.find_elements_by_tag_name("meta")
+
+    metas = {}
+    for meta in meta_tags:
+        key = meta.get_attribute("name") or meta.get_attribute("property")
+        content = meta.get_attribute("content")
+        if key:
+            metas[key.lower()] = [content]
+
+    return metas
